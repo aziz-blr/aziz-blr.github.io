475d0719ae72f72ea05d695ae2b15b824d187b34
Aziz <aziz.blore@gmail.com>
2017-02-15 21:32:12 +0300

    add

--- service-worker.js
+++ service-worker.js
@@ -1,65 +1,71 @@
 'use strict';
 
 var GCM_ENDPOINT = 'https://android.googleapis.com/gcm/send';
-//var GCM_ENDPOINT = 'https://aziz-blr.github.io/latest.json';
 
 self.addEventListener('push', function(event) {  
   // Since there is no payload data with the first version  
   // of push messages, we'll grab some data from  
-  // an APIvar httpHeaders = new Headers();
-    var httpHeaders = new Headers();
-    httpHeaders.append('pragma', 'no-cache');
-    httpHeaders.append('cache-control', 'no-cache');
+  // an API and use it to populate a notification  
+  event.waitUntil(  
+    fetch(GCM_ENDPOINT).then(function(response) {  
+      if (response.status !== 200) {  
+        // Either show a message to the user explaining the error  
+        // or enter a generic message and handle the
+        // onnotificationclick event to direct the user to a web page  
+        console.log('Looks like there was a problem. Status Code: ' + response.status);  
+        throw new Error();  
+      }
 
-    var fetchInit = {
-      method: 'GET',
-      headers: httpHeaders,
-      mode: 'no-cors',
-    };
+      // Examine the text in the response  
+      return response.json().then(function(data) {  
+        if (data.error || !data.notification) {  
+          console.error('The API returned an error.', data.error);  
+          throw new Error();  
+        }
 
-    fetch(GCM_ENDPOINT, fetchInit).then(function(res) {
-      res.json().then(function(data) {
-            // Show notification
-            console.log(data);
-            if (data.data.title == 'no') {
-              var title = 'title';
-              var message = 'body';  
-              var icon = 'https://aziz-blr.github.io/images/icon.png';  
-              var notificationTag = 'notification-error';  
-              return self.registration.showNotification(title, {  
-                body: message,  
-                icon: icon,  
-                tag: notificationTag  
-              }); 
-            }else{
-              console.log(data);
-              var title = data.data.title;
-              var message = data.data.body;  
-              var icon = data.data.icon;  
-              var notificationTag = 'notification-error';  
-              return self.registration.showNotification(title, {  
-                body: message,  
-                icon: icon,  
-                tag: notificationTag  
-              });  
-            }
-                        
-          }).catch(function(err) {  
-            console.error('Unable to retrieve data', err);
+        var title = data.notification.title;  
+        var message = data.notification.message;  
+        var icon = data.notification.icon;  
+        var notificationTag = data.notification.tag;
 
-            var title = 'An error occurred';
-            var message = 'We were unable to get the information for this push message';  
-            var icon = 'https://aziz-blr.github.io/images/icon.png';  
-            var notificationTag = 'notification-error';  
-            return self.registration.showNotification(title, {  
-              body: message,  
-              icon: icon,  
-              tag: notificationTag  
-            });  
-          })  
-        })  
+        return self.registration.showNotification(title, {  
+          body: message,  
+          icon: icon,  
+          tag: notificationTag  
+        });  
+      });  
+    }).catch(function(err) {  
+      console.error('Unable to retrieve data', err);
+
+      var title = 'An error occurred';
+      var message = 'We were unable to get the information for this push message';  
+      var icon = '';  
+      var notificationTag = 'notification-error';  
+      return self.registration.showNotification(title, {  
+          body: message,  
+          icon: icon,  
+          tag: notificationTag  
+        });  
+    })  
+  );  
 });
 
+// self.addEventListener('push', function(event) {
+//   console.log('Received a push message', event);
+
+//   var title = 'Yay a message.';
+//   var body = 'We have received a push message.';
+//   var icon = '/images/icon-192x192.png';
+//   var tag = 'simple-push-demo-notification-tag';
+
+//   event.waitUntil(
+//     self.registration.showNotification(title, {
+//       body: body,
+//       icon: icon,
+//       tag: tag
+//     })
+//   );
+// });
 
 self.addEventListener('notificationclick', function(event) {
   console.log('On notification click: ', event.notification.tag);
